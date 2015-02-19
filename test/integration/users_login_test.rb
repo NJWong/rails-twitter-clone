ae141a774f7b9229e35a76942267a0e732835776
NJWong <njwon4@gmail.com>
2015-02-19 04:59:35 +0000

    completed logging out. modified tests to test logging out

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -22,11 +22,13 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert flash.empty?  
   end
   
-  test "login with valid information" do
+  test "login with valid information followed by logout" do
     # visit the login path
     get login_path
     # post valid information to the sessions path
     post login_path, session: { email: @user.email, password: 'password' }
+    # verify that the user is logged in
+    assert is_logged_in?
     # verify we are redirected to the right target
     assert_redirected_to @user
     # visit the target page and verify the page renders correctly
@@ -38,5 +40,12 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", logout_path
     # verify that a profile link appears
     assert_select "a[href=?]", user_path(@user)
+    delete logout_path
+    assert_not is_logged_in?
+    assert_redirected_to root_url
+    follow_redirect!
+    assert_select "a[href=?]", login_path
+    assert_select "a[href=?]", logout_path, count: 0
+    assert_select "a[href=?]", user_path(@user), count: 0
   end
 end
